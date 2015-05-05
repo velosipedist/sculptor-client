b4784cb8786463bcbceb2cf803d0c2a47c23bf30
velosipedist <velosipedist.org@gmail.com>
2015-05-06 02:48:46 +0300

    update test script

--- build/test.php
+++ build/test.php
@@ -1,28 +1,25 @@
 <?php
-require_once 'sculptor_old.phar';
-require_once 'faker.phar';
-//require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
 use velosipedist\SculptorClient\exception\ApiException;
 use velosipedist\SculptorClient\Lead;
 use velosipedist\SculptorClient\SculptorClient;
 
-SculptorClient::injectClientId('#only-form');
+SculptorClient::configureLeadsJavascript([
+    'mock_ga' => true
+]);
+
 if (isset($_POST['send'])) {
     $config = require __DIR__ . '/test-config.php';
-    $api = new SculptorClient(
-        $config['api_key'],
-        $config['project_id'],
-        'post',
-        'http://test.sculptor.tochno-tochno.ru',
-        ['verify' => false]
-    );
-    $api->setDebugMode(true);
+    $api = new SculptorClient($config['api_key'], $config['project_id'], [
+        'testing' => true
+    ]);
     $api->setErrorHandler(function (ApiException $e) {
         print "<div class='alert alert-danger'>";
         print $e->getMessage();
         print "</div>";
     });
-    $result = $api->createLead(new Lead(
+    $lead = new Lead(
         $_POST['name'],
         $_POST['phone'],
         $_POST['email'],
@@ -30,8 +27,12 @@ if (isset($_POST['send'])) {
         null,
         'default',
         ['zip' => $_POST['zip']]
-    ));
-    if ($result) {
+    );
+    if ($_POST['google_client_id']) {
+        $api->setGoogleClientId(trim($_POST['google_client_id']));
+    }
+    $response = $api->createLead($lead);
+    if ($response) {
         print "<div class='alert alert-success'>Success</div>";
     }
 }
@@ -42,17 +43,8 @@ $faker = Faker\Factory::create('ru_RU');
 <head>
     <meta charset="UTF-8">
     <title>Test</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-    <script type="text/javascript">
-        //simulate Google lib
-        window.ga = function (callback) {
-            callback.apply(null, [window.ga]);
-        };
-        window.ga.get = function (anything) {
-            return 'test-client-id';
-        }
-    </script>
 </head>
 <body>
 <div class="container">
@@ -61,24 +53,33 @@ $faker = Faker\Factory::create('ru_RU');
             <br/>
 
             <div class="well">Fill me:
-                <form method="post" id="only-form">
+                <form method="post" id="only-form" data-sculptor-lead="1">
                     <div class="form-group">
                         <input class="form-control" type="text" name="name" placeholder="Name"
                                value="<?= $faker->firstName . ' ' . $faker->lastName ?>"/>
                     </div>
-                    <div class="form-group"><input class="form-control" type="text" name="phone" placeholder="Phone"
+                    <div class="form-group"><input class="form-control" type="text" name="phone"
+                                                   placeholder="Phone"
                                                    value="<?= $faker->phoneNumber ?>"/>
                     </div>
-                    <div class="form-group"><input class="form-control" type="text" name="email" placeholder="Email"
+                    <div class="form-group"><input class="form-control" type="text" name="email"
+                                                   placeholder="Email"
                                                    value="<?= $faker->email ?>"/>
                     </div>
-                    <div class="form-group"><input class="form-control" type="text" name="city" placeholder="City"
+                    <div class="form-group"><input class="form-control" type="text" name="city"
+                                                   placeholder="City"
                                                    value="<?= $faker->city ?>"/>
                     </div>
-                    <div class="form-group"><input class="form-control" type="text" name="zip" placeholder="ZIP CODE"
+                    <div class="form-group"><input class="form-control" type="text" name="zip"
+                                                   placeholder="ZIP CODE"
                                                    value="<?= $faker->postcode ?>"/>
                     </div>
+                    <div class="form-group"><input class="form-control" type="text" name="google_client_id"
+                                                   placeholder="GA Client ID override"
+                                                   value=""/>
+                    </div>
                     <input type="submit" value="Send" class="btn btn-primary" name="send"/>
+                    <a class="btn btn-default" href="/"><i class="glyphicon glyphicon-refresh"></i></a>
                 </form>
             </div>
         </div>
